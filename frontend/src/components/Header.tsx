c58100f935a225488cf8ade77bb999a7b515e368
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-30 14:52:34 +0200

    update the header to show accordingly whether the user is logged in or not

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,28 @@
 import { Link } from '@tanstack/react-router'
-
+import { useUser } from '@clerk/clerk-react'
 import ClerkHeader from '../integrations/clerk/header-user'
 
 export default function Header() {
+  const { isSignedIn } = useUser()
+  const homeOrDash = isSignedIn ? 'Dashboard' : 'Home'
   return (
     <header className="p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
+        <h1 className='px-2 font-bold'>LOGO </h1>
         <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
+          <Link to="/">{homeOrDash}</Link>
         </div>
 
-        <div className="px-2 font-bold">
-          <Link to="/demo/clerk">Clerk</Link>
-        </div>
+        {isSignedIn ? (
+          <>
+            <div className="px-2 font-bold">
+              <Link to="/logs">Logs</Link>
+            </div>
+            <div className="px-2 font-bold">
+              <Link to="/products">Products</Link>
+            </div>
+          </>
+        ) : null}
       </nav>
 
       <div>
