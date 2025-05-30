5c2f840bd357ffe7fa70f9160a0baece15dc9eec
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-30 23:04:44 +0200

    set up project

---
+++ backend/src/main/java/velma/backend/VelmaApp.java
@@ -0,0 +1,13 @@
+package velma.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelmaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelmaApp.class, args);
+	}
+
+}
---
+++ backend/src/test/java/velma/backend/VelmaAppTests.java
@@ -0,0 +1,13 @@
+package velma.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelmaAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
