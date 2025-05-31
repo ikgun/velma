79ffa27ccce3268d797b3281155f921a84b64be5
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:13:28 +0200

    update useDeleteLog to fetch auth token and send it to the api call

--- frontend/src/hooks/log/useDeleteLog.ts
+++ frontend/src/hooks/log/useDeleteLog.ts
@@ -1,10 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useAuth } from '@clerk/clerk-react'
 import { deleteLog } from '../../apis/logApi'
 
 export function useDeleteLog() {
+  const { getToken } = useAuth()
   const queryClient = useQueryClient()
   return useMutation({
-    mutationFn: deleteLog,
+    mutationFn: async (id: number) => {
+      const token = await getToken()
+      if (!token) {
+        throw new Error('No token found. User might not be authenticated.')
+      }
+      return deleteLog(id, token)
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['logs'] })
     },
