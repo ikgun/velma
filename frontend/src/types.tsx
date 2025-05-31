445d7d1f71aea868ddb70597a16ad1ea0d04f51b
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:15:49 +0200

    add id to types to be able to use with backend

--- frontend/src/types.tsx
+++ frontend/src/types.tsx
@@ -1,4 +1,5 @@
 export type Product = {
+  id: number
   name: string
   brand: string
   type: string
@@ -6,6 +7,7 @@ export type Product = {
 }
 
 export type Log = {
+  id: number
   dateTime: string
   routineType: string
   productsUsed: Array<Product>
