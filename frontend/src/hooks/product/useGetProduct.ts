c5a697d9c619435c95e81e0763b7ab54ee245eea
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:14:37 +0200

    update useGetProduct to fetch auth token and send it to the api call

--- frontend/src/hooks/product/useGetProduct.ts
+++ frontend/src/hooks/product/useGetProduct.ts
@@ -1,9 +1,17 @@
 import { useQuery } from '@tanstack/react-query'
-import { getProduct } from '../../apis/productApi'
+import { useAuth } from '@clerk/clerk-react'
+import { getProduct } from '@/apis/productApi'
 
 export function useGetProduct(id: string) {
+  const { getToken } = useAuth()
   return useQuery({
     queryKey: ['products', id],
-    queryFn: () => getProduct(id),
+    queryFn: async () => {
+      const token = await getToken()
+      if (!token) {
+        throw new Error('No token found. User might not be authenticated.')
+      }
+      return getProduct(id, token)
+    }
   })
 }
