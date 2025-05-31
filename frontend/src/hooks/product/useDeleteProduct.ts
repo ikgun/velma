0767661c01d2316cafccbd3e2d47dea6a25110d4
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:13:51 +0200

    update useDeleteProduct to fetch auth token and send it to the api call

--- frontend/src/hooks/product/useDeleteProduct.ts
+++ frontend/src/hooks/product/useDeleteProduct.ts
@@ -1,10 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { deleteProduct } from '../../apis/productApi'
+import { useAuth } from '@clerk/clerk-react'
+import { deleteProduct } from '@/apis/productApi'
 
 export function useDeleteProduct() {
+  const { getToken } = useAuth()
   const queryClient = useQueryClient()
   return useMutation({
-    mutationFn: deleteProduct,
+    mutationFn: async (id: number) => {
+      const token = await getToken()
+      if (!token) {
+        throw new Error('No token found. User might not be authenticated.')
+      }
+      return deleteProduct(id, token)
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['products'] })
     },
