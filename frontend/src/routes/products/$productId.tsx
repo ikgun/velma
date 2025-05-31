69c0bea758808e5f6cd1d4bef71218d25789208d
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 14:00:43 +0200

    check if user is signed in otherwise give message that they need to be signed in to view this page

--- frontend/src/routes/products/$productId.tsx
+++ frontend/src/routes/products/$productId.tsx
@@ -5,7 +5,19 @@ export const Route = createFileRoute('/products/$productId')({
   component: ProductPage,
 })
 
-function RouteComponent() {
-  return <div>Hello "/products/$productId"!</div>
 function ProductPage() {
+  // const { productId } = useParams({ from: '/products/$productId'});
+   const { isSignedIn, isLoaded } = useUser()
+  if (!isLoaded) {
+    return <div className="p-4">Loading...</div>
+  }
+
+  if (!isSignedIn) {
+    return <div className="p-4">Sign in to view this page</div>
+  }
+  return (
+    <SignedIn>
+      <div>Hello "/products/$productId"!</div>
+    </SignedIn>
+  )
 }
