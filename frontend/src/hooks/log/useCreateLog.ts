419228a59fed12cd7396fc01d2566fc1c2453b88
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 14:08:56 +0200

    create custom hook to call api to create a log

---
+++ frontend/src/hooks/log/useCreateLog.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useAuth } from '@clerk/clerk-react'
+import { createLog } from '../../apis/logApi'
+import type { Log } from '../../types'
+
+export function useCreateLog() {
+  const { getToken } = useAuth()
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (requestBody : Log) => {
+          const token = await getToken()
+          if (!token) {
+            throw new Error('No token found. User might not be authenticated.')
+          }
+          return createLog(requestBody, token)
+        },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['logs'] })
+    },
+    retry: 2,
+  })
+}
