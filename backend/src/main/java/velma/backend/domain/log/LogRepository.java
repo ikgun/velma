6b52f69b23ca56134007cbd9a210e603aee04bb3
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 20:49:26 +0200

    use repository method to fetch a list of logs

--- backend/src/main/java/velma/backend/domain/log/LogRepository.java
+++ backend/src/main/java/velma/backend/domain/log/LogRepository.java
@@ -1,6 +1,10 @@
 package velma.backend.domain.log;
 
 import org.springframework.data.repository.ListCrudRepository;
+import velma.backend.domain.product.Product;
+
+import java.util.List;
 
 public interface LogRepository extends ListCrudRepository<Log, Long> {
+    List<Log> findLogsByProductsUsedContaining(List<Product> productsUsed);
 }
