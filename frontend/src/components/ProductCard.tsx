c6e73ec0bc253a19a08c87132dbc85e44fd1cf09
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:17:50 +0200

    import useDeleteProduct to be able to delete a product in ProductCard

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -7,10 +7,14 @@ export default function ProductCard(){
         </>
     )
 import type { Product as ProductCardProps } from '@/types'
+import { useDeleteProduct } from '@/hooks/product/useDeleteProduct'
+
+export default function ProductCard({
   id,
   name,
   brand,
   type,
   expirationDate,
 }: ProductCardProps) {
+  const mutation = useDeleteProduct()
 }
