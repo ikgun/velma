3dd031a8b87ccfea865943a066a4726a9a71f45f
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:12:44 +0200

    update request body type in useCreateLog

--- frontend/src/hooks/log/useCreateLog.ts
+++ frontend/src/hooks/log/useCreateLog.ts
@@ -1,19 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useAuth } from '@clerk/clerk-react'
 import { createLog } from '../../apis/logApi'
-import type { Log } from '../../types'
+import type { Product } from '../../types'
 
 export function useCreateLog() {
   const { getToken } = useAuth()
   const queryClient = useQueryClient()
   return useMutation({
-    mutationFn: async (requestBody : Log) => {
-          const token = await getToken()
-          if (!token) {
-            throw new Error('No token found. User might not be authenticated.')
-          }
-          return createLog(requestBody, token)
-        },
+    mutationFn: async (requestBody: {
+      dateTime: string
+      routineType: string
+      productsUsed: Array<Product>
+      notes: string
+    }) => {
       const token = await getToken()
       if (!token) {
         throw new Error('No token found. User might not be authenticated.')
