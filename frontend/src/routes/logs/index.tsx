a75e85710a14d62fdc80df969b3d906385784aad
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 14:01:47 +0200

    check if user is signed in otherwise give message they need to be logged in to see this page

--- frontend/src/routes/logs/index.tsx
+++ frontend/src/routes/logs/index.tsx
@@ -1,16 +1,25 @@
 import { Link, createFileRoute } from '@tanstack/react-router'
+import { SignedIn, useUser } from '@clerk/clerk-react'
 
 export const Route = createFileRoute('/logs/')({
   component: LogsPage,
 })
 
 function LogsPage() {
+   const { isSignedIn, isLoaded } = useUser()
+  if (!isLoaded) {
+    return <div className="p-4">Loading...</div>
+  }
+
+  if (!isSignedIn) {
+    return <div className="p-4">Sign in to view this page</div>
+  }
   return (
-    <>
+    <SignedIn>
       <div>this is where the log history is</div>
       <div className="px-2 font-bold">
         <Link to="/logs/new">Add new log</Link>
       </div>
-    </>
+    </SignedIn>
   )
 }
