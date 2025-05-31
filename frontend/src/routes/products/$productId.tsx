fe9275133437ce317f48d595533f386c9fe43672
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 13:59:31 +0200

    import

--- frontend/src/routes/products/$productId.tsx
+++ frontend/src/routes/products/$productId.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { SignedIn, useUser } from '@clerk/clerk-react'
 
 export const Route = createFileRoute('/products/$productId')({
   component: RouteComponent,
