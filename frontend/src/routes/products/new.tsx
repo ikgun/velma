196b4c54518314d7f7847cf4c68eb3c50a5d0f99
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 14:05:20 +0200

    use useState to control fields in the form

--- frontend/src/routes/products/new.tsx
+++ frontend/src/routes/products/new.tsx
@@ -6,6 +6,10 @@ export const Route = createFileRoute('/products/new')({
 
 function AddProductFormPage() {
   const { isSignedIn, isLoaded } = useUser()
+  const [name, setName] = useState('')
+  const [brand, setBrand] = useState('')
+  const [type, setType] = useState('')
+  const [expirationDate, setExpirationDate] = useState('') // store as string YYYY-MM-DD
   if (!isLoaded) {
     return <div className="p-4">Loading...</div>
   }
@@ -14,6 +18,67 @@ function AddProductFormPage() {
     return <div className="p-4">Sign in to view this page</div>
   }
   
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    mutate({ name, brand, type, expirationDate })
+
+    if (isSuccess) {
+      setName('')
+      setBrand('')
+      setType('')
+      setExpirationDate('')
+    }
+
+    if (error) {
+      console.log(error.message)
+    }
+  }
+
+  return (
     <SignedIn>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Write name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Brand:
+          <input
+            type="text"
+            name="brand"
+            value={brand}
+            placeholder="Write your brand"
+            onChange={(e) => setBrand(e.target.value)}
+          />
+        </label>
+        <label>
+          Type:
+          <input
+            type="text"
+            name="type"
+            value={type}
+            placeholder="Write your type"
+            onChange={(e) => setType(e.target.value)}
+          />
+        </label>
+        <label>
+          Expiration date:
+          <input
+            type="date"
+            name="date"
+            value={expirationDate}
+            onChange={(e) => setExpirationDate(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Send" />
+      </form>
     </SignedIn>
+  )
 }
