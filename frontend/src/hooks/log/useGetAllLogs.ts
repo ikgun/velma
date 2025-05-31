c7d1d20e673e70ffdb17eb6f16c31ac989097718
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:14:06 +0200

    update useGetAllLogs to fetch auth token and send it to the api call

--- frontend/src/hooks/log/useGetAllLogs.ts
+++ frontend/src/hooks/log/useGetAllLogs.ts
@@ -1,9 +1,17 @@
 import { useQuery } from '@tanstack/react-query'
+import { useAuth } from '@clerk/clerk-react'
 import { getAllLogs } from '../../apis/logApi'
 
 export function useGetAllLogs() {
+  const { getToken } = useAuth()
   return useQuery({
     queryKey: ['logs'],
-    queryFn: getAllLogs,
+    queryFn: async () => {
+      const token = await getToken()
+      if (!token) {
+        throw new Error('No token found. User might not be authenticated.')
+      }
+      return getAllLogs(token)
+    }
   })
 }
