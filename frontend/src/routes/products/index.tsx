13f475849bcb9f772d6e45242d06b0d139f729ea
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 14:02:04 +0200

    check if user is signed in otherwise give message they need to be logged in to see this page

--- frontend/src/routes/products/index.tsx
+++ frontend/src/routes/products/index.tsx
@@ -1,16 +1,25 @@
 import { Link, createFileRoute } from '@tanstack/react-router'
+import { SignedIn, useUser } from '@clerk/clerk-react'
 
 export const Route = createFileRoute('/products/')({
   component: ProductsPage,
 })
 
 function ProductsPage() {
+   const { isSignedIn, isLoaded } = useUser()
+  if (!isLoaded) {
+    return <div className="p-4">Loading...</div>
+  }
+
+  if (!isSignedIn) {
+    return <div className="p-4">Sign in to view this page</div>
+  }
   return (
-    <>
+    <SignedIn>
       <div>this is where all products are</div>
       <div className="px-2 font-bold">
         <Link to="/products/new">Add new product</Link>
       </div>
-    </>
+    </SignedIn>
   )
 }
