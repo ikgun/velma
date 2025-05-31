093ccd42a5507c573eddaa2daa65a2c3b0fbb37f
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 21:13:08 +0200

    update request body type in useCreateProduct

--- frontend/src/hooks/product/useCreateProduct.ts
+++ frontend/src/hooks/product/useCreateProduct.ts
@@ -1,14 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useAuth } from '@clerk/clerk-react'
 import { createProduct } from '../../apis/productApi'
-import type { Product } from '../../types'
 
 export function useCreateProduct() {
    const { getToken } = useAuth()
   const queryClient = useQueryClient()
  
   return useMutation({
-    mutationFn: async (requestBody: Product) => {
+    mutationFn: async (requestBody: {name: string, brand: string, type: string, expirationDate: string}) => {
       const token = await getToken()
       if (!token) {
         throw new Error('No token found. User might not be authenticated.')
