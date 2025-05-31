32b555bd28d9d1a4817fac0a83c3637ea732f6fa
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-05-31 14:10:11 +0200

    create custom hook to call api to update a specific log

---
+++ frontend/src/hooks/log/useUpdateLog.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useAuth } from '@clerk/clerk-react'
+import { updateLog } from '../../apis/logApi'
+import type { Log } from '../../types'
+
+export function useUpdateLog() {
+  const { getToken } = useAuth()
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async ({
+      id,
+      requestBody,
+    }: {
+      id: string
+      requestBody: Log
+    }) => {
+      const token = await getToken()
+      if (!token) {
+        throw new Error('No token found. User might not be authenticated.')
+      }
+      return updateLog(id, requestBody, token)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['logs'] })
+    },
+    retry: 2,
+  })
+}
