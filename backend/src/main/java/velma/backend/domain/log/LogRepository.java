8ce15befe3c02cc80e1c1f4dfcf2e1a8f55437a8
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-01 18:23:27 +0200

    add new methods to LogRepository that are used by service

--- backend/src/main/java/velma/backend/domain/log/LogRepository.java
+++ backend/src/main/java/velma/backend/domain/log/LogRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.repository.ListCrudRepository;
 import velma.backend.domain.product.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LogRepository extends ListCrudRepository<Log, Long> {
     List<Log> findLogsByProductsUsedContaining(List<Product> productsUsed);
+
+    List<Log> findAllByUserId(String userId);
+
+    Optional<Log> findByUserIdAndId(String userId, Long id);
 }
