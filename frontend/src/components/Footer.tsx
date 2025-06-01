a61196011acf410b2b20772c5f591478514aa585
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-01 18:22:24 +0200

    create footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer sm:footer-horizontal footer-center bg-[#141414] text-white p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Ikbal
+          Gundogdu
+        </p>
+      </aside>
+    </footer>
+  )
+}
