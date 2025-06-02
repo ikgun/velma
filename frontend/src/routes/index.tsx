1bd172f98bb313c453c53bf7eef648f2a1913633
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 12:51:07 +0200

    refactor and design

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -17,8 +17,23 @@ function App() {
   }, [isSignedIn, navigate])
 
   if (!isLoaded) {
-    return <div className="p-4">Loading...</div>
+    return (
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        <span className="loading loading-dots loading-xl"></span>
+      </div>
+    )
   }
 
-  return <div className="p-4">You're viewing the unsigned version</div>
+  return (
+    <div className="h-screen flex flex-col bg-[#FFFFFF] font-old text-[#141414]">
+      <div className="m-10">
+        <div className="flex items-center justify-center text-center min-h-[300px]">
+          <p className="text-lg">
+            You're viewing the unsigned version, please login to start using
+            Velma!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
