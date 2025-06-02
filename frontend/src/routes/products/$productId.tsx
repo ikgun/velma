4d22b8f2092c4d0280d76fa00451bb5bbdd69c7e
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 13:27:03 +0200

    refactor and design

--- frontend/src/routes/products/$productId.tsx
+++ frontend/src/routes/products/$productId.tsx
@@ -15,71 +15,94 @@ function ProductPage() {
   const [showEditProduct, setShowEditProduct] = useState(false)
 
   if (!isLoaded) {
-    return <div className="p-4">Loading user...</div>
+    return (
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        <span className="loading loading-dots loading-xl"></span>
+      </div>
+    )
   }
 
   if (!isSignedIn) {
-    return <div className="p-4">Sign in to view this page</div>
+    return (
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        <p className="text-lg mb-4">Sign in to view this page</p>
+      </div>
+    )
   }
 
   if (isPending) {
     return (
-      <div className="flex items-center justify-center h-screen px-4">
-        <p className="text-lg text-gray-600 text-center">Loading product...</p>
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        Loading product {" "} <span className="loading loading-dots loading-xl"></span>
       </div>
     )
   }
 
   if (error) {
     return (
-      <div className="flex items-center justify-center h-screen px-4">
-        <p className="text-lg text-red-500 text-center">
-          Error: {error.message}
-        </p>
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        <p className="text-lg">{error.message}</p>
       </div>
     )
   }
 
   return (
     <SignedIn>
-      <div className="max-w-4xl mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold text-[#252422] mb-4">{data.name}</h1>
+       <div className="min-h-screen flex flex-col bg-[#FFFFFF] font-old text-[#141414]">
+        <div className="m-10">
+          <h1 className="text-2xl font-semibold mb-1">Product Details</h1>
 
-        <section>
-          <h2 className="text-lg font-semibold text-gray-700 mb-2">Brand</h2>
-          <p className="text-gray-600 text-sm ">{!(data.brand) ? "Brand not specified" : data.brand}</p>
-        </section>
+          <p className="mb-8 text-gray-700 text-sm">
+            This page shows more info about your saved product.
+          </p>
 
-        <section>
-          <h2 className="text-lg font-semibold text-gray-700 mb-2">Type</h2>
-          <p className="text-gray-600 text-sm ">{!(data.type) ? "Type not specified" : data.type}</p>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 text-gray-700">Name</h2>
+            <p className="text-gray-600 text-sm">{data.name}</p>
+          </section>
 
-        <section>
-          <h2 className="text-lg font-semibold text-gray-700 mb-2">
-            Expiration date
-          </h2>
-          <p className="text-gray-600 text-sm ">{!(data.expirationDate) ? "Expiration date not specified" : data.expirationDate}</p>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 text-gray-700">Brand</h2>
+            <p className="text-gray-600 text-sm">
+              {data.brand || 'Brand not specified'}
+            </p>
+          </section>
 
-        <button
-          className="mt-6 bg-amber-500 hover:bg-amber-600 text-black font-semibold py-2 px-4 rounded-lg transition"
-          onClick={() => setShowEditProduct(true)}
-          type="button"
-        >
-          Edit product
-        </button>
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 text-gray-700">Type</h2>
+            <p className="text-gray-600 text-sm">
+              {data.type || 'Type not specified'}
+            </p>
+          </section>
 
-        {showEditProduct && (
-          <EditProductModal
-            id={productId}
-            oldName={data.name}
-            oldBrand={data.brand}
-            oldType={data.type}
-            oldExpirationDate={data.expirationDate}
-            onClose={() => setShowEditProduct(false)}
-          />
-        )}
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 text-gray-700">
+              Expiration Date
+            </h2>
+            <p className="text-gray-600 text-sm">
+              {data.expirationDate || 'Expiration date not specified'}
+            </p>
+          </section>
+
+          <button
+            className="bg-[#141414] text-white font-semibold px-5 py-2 rounded hover:bg-[#5c5c5c] transition-colors"
+            onClick={() => setShowEditProduct(true)}
+            type="button"
+          >
+            Edit product
+          </button>
+
+          {showEditProduct && (
+            <EditProductModal
+              id={productId}
+              oldName={data.name}
+              oldBrand={data.brand}
+              oldType={data.type}
+              oldExpirationDate={data.expirationDate}
+              onClose={() => setShowEditProduct(false)}
+            />
+          )}
+        </div>
       </div>
     </SignedIn>
   )
