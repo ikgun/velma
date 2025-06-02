9a3d86c7a009946282a3dd05091da6b456231cfa
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 12:43:54 +0200

    edit sign in and up btns

--- frontend/src/integrations/clerk/header-user.tsx
+++ frontend/src/integrations/clerk/header-user.tsx
@@ -1,5 +1,6 @@
 import {
   SignInButton,
+  SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
@@ -12,7 +13,16 @@ export default function HeaderUser() {
         <UserButton />
       </SignedIn>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <button className="hover:cursor-pointer px-3 py-2 rounded transition-colors duration-200 ease-in-out hover:bg-[#333333]">
+            Log in
+          </button>
+        </SignInButton>
+        <SignUpButton>
+          <button className="hover:cursor-pointer px-3 py-2 rounded transition-colors duration-200 ease-in-out hover:bg-[#333333]">
+            Sign up
+          </button>
+        </SignUpButton>
       </SignedOut>
     </>
   )
