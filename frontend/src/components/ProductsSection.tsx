d1b47d9bac598e3daa206ff031854a5a2c8925b4
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 12:57:30 +0200

    add btns to add a new peduct or see all products in ProductsSection.tsx

--- frontend/src/components/ProductsSection.tsx
+++ frontend/src/components/ProductsSection.tsx
@@ -7,18 +7,42 @@ type ProductsSectionProps = {
   products: Array<Product>
 }
 
-export default function ProductsSection(){
-    return(
-        <>
-        <div>
-            <h1>Your products</h1>
-            <button>click to add new product</button>
-            <p>see more...</p>
-            <ProductCard/>
-        </div>
-        </>
-    )
 export default function ProductsSection({ products }: ProductsSectionProps) {
   const hasProducts = products && products.length > 0
   const lastThree = hasProducts ? products.slice(-3).reverse() : []
+
+
+      <div className="flex gap-2 w-90 sm:w-100 h-10 mb-6">
+        <Link
+          to="/products/new"
+          className="bg-[#141414] text-sm sm:text-md text-white px-4 py-1 items-center justify-center flex rounded hover:bg-gray-700 transition-colors duration-200 w-full text-center"
+        >
+          + Add New Product
+        </Link>
+
+        
+          <Link
+            to="/products"
+            className="flex items-center text-left justify-left px-4 py-1 rounded text-xs sm:text-sm font-medium text-[#141414] hover:bg-gray-200 transition-colors duration-200 block w-full"
+          >
+            See all of your products
+            <svg
+              className="w-4 h-4 ml-1"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </Link>
+        
+      </div>
+
 }
