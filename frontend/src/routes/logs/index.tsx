2609626855971e464a6d6dc97d5fc6a554e6a3c3
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 12:49:15 +0200

    use loader from daisyui

--- frontend/src/routes/logs/index.tsx
+++ frontend/src/routes/logs/index.tsx
@@ -14,6 +14,7 @@ function LogsPage() {
 
   if (!isLoaded) {
     return <div className="p-4">Loading user...</div>
+        <span className="loading loading-dots loading-xl"></span>
   }
 
   if (!isSignedIn) {
@@ -40,6 +41,7 @@ function LogsPage() {
       </div>
     )
   }
+            <span className="loading loading-dots loading-xl"></span>
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
