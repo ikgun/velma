9c070d6ef3d4b4ca3babc459233f844ae190916f
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 13:26:43 +0200

    refactor and design

--- frontend/src/routes/logs/$logId.tsx
+++ frontend/src/routes/logs/$logId.tsx
@@ -40,28 +40,25 @@ function LogPage() {
 
   if (isPending) {
     return (
-      <div className="flex items-center justify-center h-screen px-4">
-        <p className="text-lg text-gray-600 text-center">Loading log...</p>
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        Loading log {" "} <span className="loading loading-dots loading-xl"></span>
       </div>
     )
   }
 
   if (error) {
     return (
-      <div className="flex items-center justify-center h-screen px-4">
-        <p className="text-lg text-red-500 text-center">
-          Error: {error.message}
-        </p>
+      <div className="flex flex-col items-center justify-center h-screen  bg-[#FFFFFF] font-old text-[#141414] px-4 text-center">
+        <p className="text-lg">{error.message}</p>
       </div>
     )
   }
 
   return (
     <SignedIn>
-      <div className="max-w-4xl mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold text-[#252422] mb-4">
-          {data.dateTime}
-        </h1>
+       <div className="min-h-screen flex flex-col bg-[#FFFFFF] font-old text-[#141414]">
+        <div className="m-10">
+          <h1 className="text-2xl font-semibold mb-1">Log Entry</h1>
 
           <p className="mb-8 text-gray-700 text-sm">
             Created at {formatCustomDate(data.dateTime)}
@@ -84,38 +81,39 @@ function LogPage() {
             )}
           </section>
 
-        <section>
-          <h2 className="text-lg font-semibold text-gray-700 mb-2">
-            Routine type
-          </h2>
-          <p className="text-gray-600 text-sm ">{data.routineType}</p>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 text-gray-700">
+              Routine Type
+            </h2>
+            <p className="text-gray-600 text-sm">{data.routineType}</p>
+          </section>
 
-        <section>
-          <h2 className="text-lg font-semibold text-gray-700 mb-2">
-            Notes
-          </h2>
-          <p className="text-gray-600 text-sm ">{!(data.notes) ? "Nothing here..." : data.notes}</p>
-        </section>
+          <section className="mb-8">
+            <h2 className="text-lg font-semibold mb-2 text-gray-700">Notes</h2>
+            <p className="text-gray-600 text-sm">
+              {data.notes ? data.notes : 'Nothing here...'}
+            </p>
+          </section>
 
-        <button
-          className="mt-6 bg-amber-500 hover:bg-amber-600 text-black font-semibold py-2 px-4 rounded-lg transition"
-          onClick={() => setShowEditLog(true)}
-          type="button"
-        >
-          Edit log
-        </button>
+          <button
+            className="bg-[#141414] text-white font-semibold px-5 py-2 rounded hover:bg-[#5c5c5c] hover:cursor-pointer transition-colors"
+            onClick={() => setShowEditLog(true)}
+            type="button"
+          >
+            Edit log
+          </button>
 
-        {showEditLog && (
-          <EditLogModal
-            id={logId}
-            oldDateTime={data.dateTime}
-            oldRoutineType={data.routineType}
-            oldProductsUsed={data.productsUsed}
-            oldNotes={data.notes}
-            onClose={() => setShowEditLog(false)}
-          />
-        )}
+          {showEditLog && (
+            <EditLogModal
+              id={logId}
+              oldDateTime={data.dateTime}
+              oldRoutineType={data.routineType}
+              oldProductsUsed={data.productsUsed}
+              oldNotes={data.notes}
+              onClose={() => setShowEditLog(false)}
+            />
+          )}
+        </div>
       </div>
     </SignedIn>
   )
