4c01cea0ae6c26153dd16a70febd3ec01a7bb6c7
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-02 09:23:29 +0200

    add new method to ProductRepository to use in the service

--- backend/src/main/java/velma/backend/domain/product/ProductRepository.java
+++ backend/src/main/java/velma/backend/domain/product/ProductRepository.java
@@ -2,5 +2,11 @@ package velma.backend.domain.product;
 
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends ListCrudRepository<Product, Long> {
+    Optional<Product> findByUserIdAndId(String userId, Long id);
+
+    List<Product> findAllByUserId(String userId);
 }
