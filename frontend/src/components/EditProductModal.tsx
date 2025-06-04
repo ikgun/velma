ce7fe31ca1a434ce0e585206f61365b284a3c0ee
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-04 08:13:04 +0200

    remove unused statement

--- frontend/src/components/EditProductModal.tsx
+++ frontend/src/components/EditProductModal.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 import type { FormEvent } from 'react'
