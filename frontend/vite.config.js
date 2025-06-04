606e6a59983461bffdb019da6abfd9ccbd0233ef
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-04 13:52:18 +0200

    update vite.config.js for docker hot load

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,37 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { resolve } from "node:path";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
-  }
-});
+  },
+  server: {
+    watch: {
+      // Use polling, with a reasonable interval in ms
+      usePolling: true,
+      interval: 1000,
+    },
+    fs: {
+      // Allow serving files from outside the root if needed
+      strict: false,
+    },
+    host: true, // equivalent to --host
+    port: 3000,
+  },
+})
